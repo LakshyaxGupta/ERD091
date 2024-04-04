fa06b516ba08aafa10c8c7886d19b1372cd4f10b
LakshyaxGupta <149102616+LakshyaxGupta@users.noreply.github.com>
2024-04-04 13:43:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const menuMobile = document.querySelector(".menu-mobile");
+const overlay = document.querySelector("#overlay");
+
+const openMenu = event => {
+  event.currentTarget.classList.add("hide");
+  close.classList.remove("hide");
+  menuMobile.classList.remove("hide");
+  overlay.style.display = "block";
+};
+
+const closeMenu = event => {
+  close.classList.add("hide");
+  hamburger.classList.remove("hide");
+  menuMobile.classList.add("hide");
+  overlay.style.display = "none";
+};
+
+hamburger.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const topContentAnimation = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".content-top",
+      start: "center 80%",
+      end: "center 20%",
+      defaults: {
+        duration: 2,
+        ease: "power1.inOut"
+      }
+    }
+  });
+
+  tl.from(".content-top", { x: "-150%", duration: 1 });
+  tl.from(".box", {
+    x: "10%",
+    opacity: 0,
+    scale: 0,
+    duration: 1.5,
+    stagger: 1
+  });
+};
+
+const articlesAnimation = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".articles",
+      start: "top 80%",
+      defaults: {
+        ease: "power1.inOut"
+      }
+    }
+  });
+
+  tl.from(".articles h2", { x: "-150%", duration: 1 });
+  tl.from(".article", {
+    x: "150%",
+    opacity: 0,
+    duration: 1.5,
+    stagger: 1
+  });
+};
+
+topContentAnimation();
+articlesAnimation();
